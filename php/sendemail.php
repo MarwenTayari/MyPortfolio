3673a4df068a569401289370e58f170b29c7ec00
MarwenTayari <tayarimarwen@gmail.com>
2022-02-02 20:11:30 +0100

    To deploy

--- php/sendemail.php
+++ php/sendemail.php
@@ -4,7 +4,6 @@ use PHPMailer\PHPMailer\PHPMailer;
 require_once 'phpmailer/Exception.php'; 
 require_once 'phpmailer/PHPMailer.php'; 
 require_once 'phpmailer/SMTP.php'; 
-require_once '../style/sendemail.css'; 
 
 $mail = new PHPMailer(true);  
 
